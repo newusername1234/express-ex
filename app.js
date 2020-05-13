0a5e0d1e1c22cdca64a830e13a2e6849c95da9ec
Wes <wesleyayer92@gmail.com>
2020-05-13 14:24:39 -0400

    preserve original data for / get

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const { header, legal, about, footer, data } = require('./routes');
+let { header, legal, about, footer, data } = require('./routes');
 const express = require('express');
 const app = express();
 const PORT = 8000;
 
 app.get('/cats', (req, res) => {
-    res.json(data.filter(animal => animal.type == 'cat'))
+    res.json(data.filter(animal => animal.type == 'cat'));
 })
 
 app.get('/dogs', (req, res) => {
     res.json(data.filter(animal => animal.type == 'dog'))
+})
+
+app.get('/:id(\\d+)', (req, res) => {
+    res.json(data.filter(animal => animal.id == req.params.id))
+})
+
+app.get('/', (req, res) => {
+    let filteredData = [...data];
+    if (req.query.color) {
+        let color = req.query.color;
+        filteredData = filteredData.filter(animal => animal.color == color);
+    } if (req.query.age) {
+        let age = req.query.age;
+        filteredData = filteredData.filter(animal => animal.age == age);
+    } if (filteredData != data) {
+        res.json(filteredData);
+    } else {
+        res.json(data);
+    }
 })
 
 app.get('/legal/:page', (req, res) => {
